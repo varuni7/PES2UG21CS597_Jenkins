5c1a082484b13f372852fc245d1b0538aea0f09e
Himank101 <b.himank101@gmail.com>
2024-03-11 09:27:00 +0530

    add files

---
+++ main/task5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello,Main\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Pipeline Built\n";
+  cout << "My srn is PES2UG21CS597\n" 
+  return 0;
+}
